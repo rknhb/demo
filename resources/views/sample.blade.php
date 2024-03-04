82a99347caa24450cdf7f203d09d12e68e9cd7ed
rknhb <raikizunanohachan@gmail.com>
2024-03-04 16:42:40 +0900

    commit

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -1,30 +1,263 @@
 <!DOCTYPE html>
-<html class="lenis lenis-smooth wf-dnp-shuei-gothic-gin-std-n4-active wf-dnp-shuei-gothic-gin-std-n5-avtive wf-dnp-shuei-gothic-gin-std-n6-active wf-active" lang="ja">
+<html class="js lenis lenis-smooth wf-dnp-shuei-gothic-gin-std-n4-active wf-dnp-shuei-gothic-gin-std-n5-active wf-dnp-shuei-gothic-gin-std-n6-active wf-active" lang="ja">
 <head>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <meta chaset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content>
     <script src="https://kit.fontawesome.com/5b69d558cb.js" crossorigin="anonymous"></script>
+    <style data-shopify>
+        :root, color-backgroud-1 {
+            --color-backgroud: 255,255,255;
+            
+            --gradient-backgroud: #ffffff;
+
+            --color-foreground: 18,18,18;
+            --color-shadow: 18,18,18;
+            --color-button: 18,18,18;
+            --color-button-text: 255,255,255;
+            --color-secondary-button: 255,255,255;
+            --color-link: 18,18,18;
+            --color-badge-foreground: 18,18,18;
+            --color-badge-background: 255,255,255;
+            --color-badge-border: 18,18,18;
+            --payment-terms-background-color: rgb(255 255 255);
+        }
+
+        body, .color-background-1, .color-background-2, .color-inverse, .color-accent-1, .color-accent-2 {
+            color: rgba(var(--color-foreground), 0.75);
+            background-color: rgb(var(--color-backgroud));
+        }
+
+        :root {
+            --font-body-family: Assistant, sans-serif;
+            --font-body-style: normal;
+            --font-body-weight: 400;
+            --font-body-weight-bold: 700;
+
+            --font-heading-family: Assistant, sans-serif;
+            --font-heading-style: normal;
+            --font-heading-weight: 400;
+
+            --font-body-scale: 1.0;
+            --font-heading-scale: 1.0;
+
+            --media-padding: px;
+            --media-border-opacity: 0.05;
+            --media-border-width: 1px;
+            --media-radius: 0px;
+            --media-shadow-opacity: 0.0;
+            --media-shadow-horizontal-offset: 0px;
+            --media-shadow-vartical-offset: 4px;
+            --media-shadow-blur-radius: 5px;
+            --media-shadow-visible: 0;
+
+            --page-width: 120rem;
+            --page-width-margin: 0rem;
+
+            --product-card-image-padding: 0.0rem;
+            --product-card-center-radius: 0.0rem;
+            --product-card-text-alignment: left;
+            --product-card-border-width: 0.0rem;
+            --product-card-border-opacity: 0.1;
+            --product-card-shadow-opacity: 0.0;
+            --product-card-shadow-visible: 0;
+            --product-card-shadow-horizontal-offset: 0.0rem;
+            --product-card-shadow-vertical-offset; 0.4rem;
+            --product-card-shadow-blur-radius: 0.5rem;
+
+            --collection-card-image-padding: 0.0rem;
+            --collection-card-corner-radius: 0.0rem;
+            --collection-card-text-alignment: left;
+            --collection-card-border-width: 0.0rem;
+            --collection-card-border-opacity: 0.1;
+            --collection-card-shadow-opacity: 0.0;
+            --collection-card-shadow-visible: 0;
+            --collection-card-shadow-horizontal-offset: 0.0rem;
+            --collection-card-shadow-vertical-offset: 0.4rem;
+            --collection-card-shadow-blur-radius: 0.5rem;
+
+            --blog-card-image-padding: 0.0rem;
+            --blog-card-corner-radius: 0.0rem;
+            --blog-card-text-alignment: left;
+            --blog-card-border-width: 0.0rem;
+            --blog-card-border-opacity: 0.1;
+            --blog-card-shadow-opacity: 0.0;
+            --blog-card-shadow-visible: 0;
+            --blog-card-shadow-horizontal-offset: 0.0rem;
+            --blog-card-shadow-vertical-offset: 0.4rem;
+            --blog-card-shadow-blur-radius: 0.5rem;
+
+            --badge-corner-radius: 4.0rem;
+
+            --popup-border-width: 1px;
+            --popup-border-opacity: 0.1;
+            --popup-corner-radius: 0px;
+            --popup-shadow-opacity: 0.05;
+            --popup-shadow-horizontal-offset: 0px;
+            --popup-shadow-vertical-offset: 4px;
+            --popup-shadow-blur-radius: 5px;
+
+            --drawer-border-width: 1px;
+            --drawer-border-opacity: 0.1;
+            --drawer-shadow-opacity: 0.0;
+            --drawer-shadow-horizontal-offset: 0px;
+            --drawer-shadow-vertical-offset: 4px;
+            --drawer-shadow-blur-radius: 5px;
+
+            --spacing-sections-desktop: 0px;
+            --spacing-sections-mobile: 0px;
+
+            --grid-desktop-vartical-spacing: 8px;
+            --grid-desktop-horizontal-spacing: 8px;
+            --grid-mobile-vartical-spacing: 4px;
+            --grid-mobile-horizontal-spacing: 4px;
+
+            --text-boxes-border-opacity: 0.1;
+            --text-boxes-border-width: 0px;
+            --text-boxes-radius: 0px;
+            --text-boxes-shadow-opacity: 0.0;
+            --text-boxes-shadow-visible: 0;
+            --text-boxes-shadow-horizontal-offset: 0px;
+            --text-boxes-shadow-vertical-offset: 4px;
+            --text-boxes-shadow-blur-radius: 5px;
+
+            --buttons-radius: 0px;
+            --buttons-radius-outset: 0px;
+            --buttons-border-width: 1px;
+            --buttons-border-opacity: 1.0;
+            --buttons-shadow-opacity: 0.0;
+            --buttons-shadow-visible: 0;
+            --buttons-shadow-horizontal-offset: 0px;
+            --buttons-shadow-vertical-offset: 4px;
+            --buttons-shadow-blur-radius: 5px;
+            --buttons-border-offset: 0px;
+
+            --inputs-radius: 0px;
+            --inputs-border-width: 1px;
+            --inputs-border-opacity: 0.55;
+            --inputs-shadow-opacity: 0.0;
+            --inputs-shadow-horizontal-offset: 0px;
+            --inputs-margin-offset: 0px;
+            --inputs-shadow-vertical-offset: 4px;
+            --inputs-shadow-blur-radius: 5px;
+            --inputs-radius-outset: 0px;
+
+            --variant-pills-radius: 40px;
+            --variant-pills-border-width: 1px;
+            --variant-pills-border-opacity: 0.55;
+            --variant-pills-shadow-opacity: 0.0;
+            --variant-pills-shadow-horizontal-offset: 0px;
+            --variant-pills-shadow-vertical-offset: 4px;
+            --variant-pills-shadow-blur-radius: 5px;
+        }
+
+        *, *::before, *::after {
+            box-sizing: inherit;
+        }
+
+        html {
+            box-sizing: border-box;
+            font-size: calc(var(--font-body-scale) * 62.5%);
+            height: 100%;
+        }
+
+        body {
+            display: grid;
+            grid-template-rows: auto auto 1fr auto;
+            grid-template-columns: 100%;
+            min-height: 100%;
+            margin: 0;
+            font-size: 1.5rem;
+            letter-spacing: 0.06rem;
+            line-height: calc(1 + 0.8 / var(--font-body-scale));
+            font-family: var(--font-body-family);
+            font-style: var(--font-body-style);
+            font-weight: var(--font-body-weight);
+        }
+
+        @media screen and (min-width: 750px) {
+            body {
+                font-size: 1.6rem;
+            }
+        }
+    </style>
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/splide-core.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="all">
 </head>
-<body>
-    <div class="container">
-        <section class="recommend">
-            <div class="parts-recommend splide is-overflow is-initialized splide--loop splide--ltr splide--draggable is-active" id="splide01" role="region" aria-roledescription="carousel">
-                <div class="parts-recommend__slider-wrap splide__track splide__track--loop splide__track--ltr splide__track--draggable" slide-area="section" id="splide01-track" style="padding-left: 0px; padding-right: 0px;" aria-live="off" aria-atomic="true" aria-busy="false">
-                    <div class="parts-recommend__slider splide__list" slide-area="target" id="splide01-list" role="presentation" style="transform: translateX(-1833.56px); transition: transform 1000ms cubic-bezier(0.25, 1, 0.5, 1) 0s;">
-                        @foreach($stocks as $stock)
-                        <div class="parts-recommend__item splide__slide splide__slide--clone" id="splide01-clone01" style="margin-right: calc(45 * var(--base-fs)); width: calc((100% + calc(45 * var(--base-fs)))/1 - calc(45 * var(--base-fs)));">
-                            <div class="parts-recommend__item__header">
-                                <p class="parts-recommend__item__title">
-                                    <span class="split-text" style="white-space: nowrap; display: inline-block; --back-size: 1;">
-                                        {{$stock->name}}
-                                    </span>
-                                </p>
+<body class="gradient animate--hover-default">
+    <main id="MainContent" class="content-for-layout focus-none" role="main" tabindex="-1">
+        <div class="container">
+            <div class="opening-cover"></div>
+            <section class="recommend">
+                <div class="parts-recommend splide is-overflow is-initialized splide--loop splide--ltr splide--draggable is-active" id="splide01" role="region" aria-roledescription="carousel">
+                    <div class="parts-recommend__slider-wrap splide__track splide__track--loop splide__track--ltr splide__track--draggable" slide-area="section" id="splide01-track" style="padding-left: 0px; padding-right: 0px;" aria-live="off" aria-atomic="true" aria-busy="false">
+                        <button class="parts-recommend__slider-btn" slide-area="prev" hover-type="opacity">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="106" fill="none" viewBox="0 0 30 106">
+                                <path fill="#081122" fill-rule="evenodd" d="M29.023.06a.5.5 0 0 1 .202.678L1.136 52.783l28.09 52.045a.5.5 0 1 1-.88.475L0 52.783 28.345.263a.5.5 0 0 1 .678-.203Z" clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                        <button class="parts-recommend__slider-btn" slide-area="next" hover-type="opacity">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="107" fill="none" viewBox="0 0 30 107">
+                                <path fill="#081122" fill-rule="evenodd" d="M.763.56a.5.5 0 0 1 .677.203l28.345 52.52-28.345 52.52a.5.5 0 0 1-.88-.475l28.09-52.046L.56 1.238A.5.5 0 0 1 .763.56Z" clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                        <div class="parts-recommend__slider splide__list" slide-area="target" id="splide01-list" role="presentation">
+                            @foreach($stocks as $stock)
+                            <div class="parts-recommend__item splide__slide splide__slide--clone" id="splide01-clone01" role="group" aria-roledescription="slide" aria-label="4 of 4" style="margin-right: calc(76 * var(--base-fs)); width: calc((100% + calc(76 * var(--base-fs)))/1 - calc(76 * var(--base-fs)));">
+                                <div class="parts-recommend__item__header">
+                                    <p class="parts-recommend__item__title">
+                                        <span class="split-text" style="white-space: nowrap; display: inline-block; --back-size: 1;">
+                                            {{$stock->name}}
+                                        </span>
+                                    </p>
+                                </div>
+                                <a class="parts-recommend__item__kv" href="" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px);" tabindex="-1">
+                                    <img src="/image/{{$stock->imagePath}}">
+                                    <div class="parts-recommend__item__kv-cover">
+                                        <span>View more</span>
+                                    </div>
+                                </a>
+                                <div class="parts-recommend__item__info" style="opacity: 1; translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
+                                    <div class="parts-recommend__item__info-bar"></div>
+                                    <div class="parts-recommend__item__info-left">
+                                        <p class="parts-recommend__item__price">
+                                            ¥{{$stock->fee}}
+                                            <span>（税込）</span>
+                                        </p>
+                                    </div>
+                                    <div class="parts-recommend__item__info-right">
+                                        <form class="parts-add-bag" action="addMyCart" method="post">
+                                            @csrf
+                                            <input type="hidden" name="stockId" value="{{ $stock->id }}" tabindex="-1">
+                                            <div class="parts-add-bag__count">
+                                                <label for="count" class="parts-add-bag__count-label">数量</label>
+                                                <div class="parts-add-bag__count-bar"></div>
+                                                <label for="count" class="parts-add-bag__count-label">1</label>
+                                            </div>
+                                            <button class="parts-add-bag__bag-add" type="submit" hover-type="fill" tabindex="-1">カートに入れる</button>
+                                        </form>
+                                        <p></p>
+                                    </div>
+                                </div>
                             </div>
+                            @endforeach
                         </div>
-                        @endforeach
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
+            </section>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1.0.19/bundled/lenis.min.js"></script>
+    <script>
+        const lenis = new Lenis();
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+    </script>
 </body>
 </html>
